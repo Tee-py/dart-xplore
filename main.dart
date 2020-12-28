9bf6b610e50a4726c868a236278bff801392c9bb
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2020-12-28 16:50:41 +0100

    done with typecasting

--- main.dart
+++ main.dart
@@ -6,13 +6,27 @@ main() {
   stdout.writeln("What is your name??");
   String name = stdin.readLineSync();
   func(name);
+  typeCasting();
 }
 
-func(String name) {
+//Tee-py: Trying to get familiar with DataTypes in DartLang.
+void func(String name) {
   var age = 34;
   int number = 567;
   double score = 35.67;
   bool isStundent = true;
   print(
       "I am $name. I am $age years and number $number. I scored $score and my student status is $isStundent");
+}
+
+void typeCasting() {
+  //string to double
+  double score_2 = double.parse("3456.6785");
+
+  //double to string
+  String d2s = 2345.7654.toStringAsFixed(2);
+
+  //Tests
+  assert(d2s == 2345.76);
+  assert(score_2 == 3456.6785);
 }
