6d8678ea2585973f6ecef97f868f0237902bc73e
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2020-12-28 16:42:13 +0100

    datatypes in dart

--- main.dart
+++ main.dart
@@ -1,7 +1,18 @@
+//Tee-py: Created This project to get familiar with Dart before the year 2020 ends.
+
 import 'dart:io';
 
 main() {
   stdout.writeln("What is your name??");
   String name = stdin.readLineSync();
-  print('my name is $name');
+  func(name);
+}
+
+func(String name) {
+  var age = 34;
+  int number = 567;
+  double score = 35.67;
+  bool isStundent = true;
+  print(
+      "I am $name. I am $age years and number $number. I scored $score and my student status is $isStundent");
 }
