b767deb81a7b6c84702faba0cb37aaba34be797f
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2020-12-28 17:57:21 +0100

    oop in dart

--- main.dart
+++ main.dart
@@ -7,6 +7,10 @@ main() {
   String name = stdin.readLineSync();
   func(name);
   typeCasting();
+
+  Animal dog = new Animal();
+  dog.setName("PitBull");
+  print(dog.toString());
 }
 
 //Tee-py: Trying to get familiar with DataTypes in DartLang.
@@ -29,4 +33,28 @@ void typeCasting() {
   //Tests
   assert(d2s == 2345.76);
   assert(score_2 == 3456.6785);
+}
+
+class Animal {
+  static String type = "Mammal";
+
+  String _name;
+
+  static String info() {
+    return "This is an animal class with type $type";
+  }
+
+  @override
+  String toString() {
+    return "I am ${this.getName()} ";
+  }
+
+  String getName() {
+    return this._name;
+  }
+
+  void setName(String name) {
+    this._name = name;
+    print("Name has been changed to $name. ");
+  }
 }
